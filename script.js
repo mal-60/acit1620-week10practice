b0573665597d2418c988a85bc643719137db4dd9
Anh <minhanhle@hotmail.com>
2022-11-08 10:26:41 -0800

    number input

--- script.js
+++ script.js
@@ -2,16 +2,33 @@
 // console.log("Hello");
 
 
-for (let i = 1; i <= 10; i++) {
-    console.log(2 ** 1)
+// for (let i = 1; i <= 10; i++) {
+//     console.log(2 ** 1)
+// }
+
+
+// //     i ** 2 ++i
+// // console.log(x)
+
+// let i = 1;
+// while (i <= 10) {
+//     console.log(2 **1)
+//     i++;
+// }
+
+let input = "";
+let numInput = "";
+do{
+input = prompt("What is your name?");
+    // prompt always returns string
+// console.log(Number(input));
+numInput = Number(input);
 }
+// we need to convert the string to a number and check if ti was successfully converted
+// while(// user enters a number)
+while(!isNaN(numInput))
+console.log(input);
+
+while(Number(input) || input == 0)
 
 
-//     i ** 2 ++i
-// console.log(x)
-
-let i = 1;
-while (i <= 10) {
-    console.log(2 **1)
-    i++;
-}
