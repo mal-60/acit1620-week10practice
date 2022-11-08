ecb0f395ffd6fb7077ab779246f945f021194a91
Anh <minhanhle@hotmail.com>
2022-11-08 09:56:04 -0800

    practice for loop and while loop

---
+++ script.js
@@ -0,0 +1,17 @@
+// alert("The rest of the page wouldn't load until you dismiss me!")
+// console.log("Hello");
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(2 ** 1)
+}
+
+
+//     i ** 2 ++i
+// console.log(x)
+
+let i = 1;
+while (i <= 10) {
+    console.log(2 **1)
+    i++;
+}
